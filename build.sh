692605c9527bf1fd9a67d20555d7105d059849a8
erfann31 <erfannasri2@gmail.com>
2024-04-01 09:11:47 +0330

    add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+
+# exit on error
+set -o errexit
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py makemigrations
+python manage.py migrate
+#python manage.py runserver
+
+python manage.py create_default_superuser
