7fe788f15340393de346ec0a422cceb459556d7f
erfann31 <erfannasri2@gmail.com>
2024-05-01 04:25:36 +0330

    fix models.py

--- task/models.py
+++ task/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -14,8 +14,8 @@ class Task(models.Model):
 
     title = models.CharField(max_length=100)
     description = models.TextField()
-    due_date = models.DateField()
-    tag = models.CharField(max_length=50)
+    due_date = models.DateField(null=True, blank=True)
+    tag = models.CharField(max_length=50, null=True, blank=True)
     completed = models.BooleanField(default=False)
     priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='0')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
