8eb3414304df79ba990dc73d71ac627f701bdfbd
erfann31 <erfannasri2@gmail.com>
2024-05-01 11:02:08 +0330

    add edit task view

--- task/views.py
+++ task/views.py
@@ -1,8 +1,9 @@
 # tasks/views.py
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -17,6 +18,26 @@ class TaskCreateAPIView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def patch(request, task_id):
+        try:
+            task = Task.objects.get(id=task_id, user=request.user)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if task:
+            serializer = TaskSerializer(task, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class TaskCompleteAPIView(APIView):
